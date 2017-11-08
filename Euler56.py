4dc60e053b8e64dea204e98c6d268be80c5d0ce0
Yash Desai <31972221+Yashmnash@users.noreply.github.com>
2017-11-08 14:25:41 -0500

    Add files via upload

---
+++ Euler56.py
@@ -0,0 +1,26 @@
+# Project Euler Problem 56: Powerful digit sum
+# By Yash Desai
+
+from time import time
+
+start = time()
+largest = 0
+
+
+# Finding the maximum digital sum of the value of a^b
+# where a and b are two natural numbers below 100.
+for a in range(1,100):
+    for b in range(1,100):
+
+        digits = [int(d) for d in str(a**b)]
+
+        candidate = sum(Digits)
+
+        if candidate > largest:
+            largest = candidate
+
+
+print('The answer is %s, found in %s seconds' % (largest,
+        time() - start))
+            
+        
